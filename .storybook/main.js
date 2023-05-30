553db215bc03a4f0c2e522fc5f27adc8739d8223
Dmitriy <dima.komlyk@gmail.com>
2023-05-30 15:25:41 +0300

    build: implement storybook

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+const config = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
